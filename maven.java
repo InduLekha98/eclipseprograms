97672bdc134d7c5f5f9b43232821b10771118d0b
InduLekha98 <rindulekha09@gmail.com>
2021-02-07 14:46:51 +0530

    maven

---
+++ maven.java
@@ -0,0 +1,12 @@
+package bridgelabs.javasamples;
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+public static void main( String[] args )
+    {
+     System.out.println( "Hello world!" );
+    }
+}
